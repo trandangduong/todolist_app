0b3cf7ac2f01df50349e327ab4dc7298f6a62ad1
Duong Tran <duongtran.bus@gmail.com>
2024-04-02 14:01:40 +0700

    update note

--- lib/note.dart
+++ lib/note.dart
@@ -4,3 +4,14 @@
 // git commit -m "comment"
 // git log
 // git remote and origin https://github.com/trandangduong/todolist_app
+// git push
+
+// de sua code >
+// git add .
+// git commit -m "da sua xong"
+// git push
+
+// clone code ve
+// git clone https://github.com/trandangduong/todolist_app
+
+
